5755c94afb02d1186e72380b3e8b24a60f39228c
Vitalii Lazuka <vlazuka@e1z2r3p5.42.us.org>
2019-11-02 22:36:59 -0700

    Hostname and username are added

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,54 @@
 
 #include "../headers/header.hpp"
 
+
+
+#include "printf.h"
+
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <limits.h>
+
+std::string	get_hostname()
+{
+	#define BUFSIZE 256
+    char buf[BUFSIZE];
+    size_t buflen = BUFSIZE;
+
+    if(gethostname(buf, buflen) == 0)
+    {
+    	std::string someString(buf);
+        return (someString);
+    }
+
+    return "";
+}
+
+
+
+std::string	get_username()
+{
+	char *c;
+	c = getlogin();
+
+	std::string someString(c);
+	// printf("%s \n", someString.c_str());
+
+    return (someString);
+
+}
+
+
+
 int		main(void)
 {
+	//////////////// HOSTNAME + USERNAME ///////////////
+	printf("%s\n", get_hostname().c_str());
+	printf("%s\n", get_username().c_str());
+	////////////////////////////////////////////////////
+	
 
 	return (0);
 }
