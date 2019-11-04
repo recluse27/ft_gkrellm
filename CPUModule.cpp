1c048c0e6a862b9bda28e5cd55613a6938635b6f
Iskander Iskakov <tradeshells@gmail.com>
2019-11-03 21:39:21 -0800

    fd

---
+++ CPUModule.cpp
@@ -0,0 +1,28 @@
+#include "CPUModule.hpp"
+#include "includes.hpp"
+
+CPUModule::CPUModule(void)
+{
+	
+}
+
+CPUModule::~CPUModule(void)
+{
+	
+}
+
+void	CPUModule::refreshData(void)
+{
+    char		*buf;
+	buf = new char(CPU_BUF_SIZE);
+	std::string	ret;
+    flag_text = 1;
+    FILE* top = popen("ps -axro pcpu | awk '{sum+=$1} END {print sum}'", "r");
+    if (top)
+        fgets(buf, CPU_BUF_SIZE, top);
+    pclose(top);
+    // return buf;
+	ret = std::string(buf);
+    data_t = "CPU:";
+    data_n = atoi(ret.c_str());
+}
---
+++ IMonitorDisplay.cpp
@@ -0,0 +1,14 @@
+#include "IMonitorDisplay.hpp"
+
+IMonitorDisplay::IMonitorDisplay(void)
+{
+	
+}
+
+void IMonitorDisplay::addModule(IMonitorModule *module){
+    modules.push_back(module);
+}
+IMonitorDisplay::~IMonitorDisplay(void)
+{
+	
+}
